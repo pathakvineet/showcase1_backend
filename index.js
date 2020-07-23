9b4ce91e1f87bdf51bbcc7f8830c7c8a4a943a5c
Vineet Pathak <pathakvineet3@gmail.com>
2020-07-23 16:46:00 +0530

    test invalid api error

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 var errorHandling = require('./controller/errors.js');
 const fileUpload = require('express-fileupload');
 const seedSportToDB = require('./seedSportData');
+const moment = require('moment'); 
 const port = process.env.PORT || 8000;
 
 const app = express();
@@ -21,7 +22,13 @@ connectDB();
 app.use(cors({origin:true, credentials:true}));
 
 //-------------mount API Routes-------------------
+app.use(function (req, res, next) {
+  // console.log(`${req.ip} :: ${req.headers['user-agent']} accessed api :: time: + ${moment().format("DD MMM,YY")} + accessed: ${req.method} + --> ${req.url.toString()}`);
+  console.log(`# ${moment().format("MMMM Do YYYY, h:mm:ss a")} || ${req.method}-> ${req.url.toString()}`);
+  next();
+});
 app.use('/api/v1/', require('./router/v1_Routes'));
+
 app.route('/*').all(errorHandling.notFoundError);
 
 
